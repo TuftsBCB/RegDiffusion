00305116fc08d5727b0f56b130829665d368a2e4
Hao Zhu <haozhu233@gmail.com>
2024-03-21 23:24:33 -0400

    fix some incorrect init

--- regdiffusion/__init__.py
+++ regdiffusion/__init__.py
@@ -1,10 +1,14 @@
 """
 Single-cell Gene Regulatory Networks Inference and Analytics
 """
+from . import data
+from . import models
 
 from .logger import LightLogger, load_logger
 from .trainer import RegDiffusionTrainer
 from .grn import GRN, read_hdf5
 from .evaluator import GRNEvaluator
 
-from regdiffusion import data as data
+__all__ = ['data', 'models', 'LightLogger', 'load_logger', 
+           'RegDiffusionTrainer', 'GRN', 'read_hdf5', 'GRNEvaluator']
+

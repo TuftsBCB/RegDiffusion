a7f04efba61174ebcec2a55758fdd378cbd493ce
haozhu233 <haozhu233@gmail.com>
2026-01-29 01:28:17 -0500

    tiny clean up before update

--- regdiffusion/plot/__init__.py
+++ regdiffusion/plot/__init__.py
@@ -1,1 +1,15 @@
 from .pyvis import plot_pyvis
+from .static import (
+    plot_network,
+    plot_heatmap,
+    plot_degree_distribution,
+    plot_training_curves
+)
+
+__all__ = [
+    'plot_pyvis',
+    'plot_network',
+    'plot_heatmap',
+    'plot_degree_distribution',
+    'plot_training_curves'
+]
